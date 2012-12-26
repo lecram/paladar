9b08b6a3bbf6d12c391c166e6e01672860c21d3d
Marcel Rodrigues <marcelgmr@gmail.com>
2012-12-26 15:47:24 -0200

    Single quotes in localized strings enables localized HTML attributes.

--- gentpl.py
+++ gentpl.py
@@ -6,13 +6,13 @@ def build():
     gentpl()
 
 def gentpl():
-    exp = r's/\[\[\([^]]*\)\]\]/{{_("\1")}}/g'
+    exp = r"'s/\[\[\([^]]*\)\]\]/{{_(\x27\1\x27)}}/g'"
     fs = os.listdir("views")
     ptps = filter(lambda f: os.path.splitext(f)[1] == ".ptp", fs)
     for ptp in ptps:
         ptppath = os.path.join("views", ptp)
         tplpath = os.path.join("views", os.path.splitext(ptp)[0] + ".tpl")
-        run('sed', "'" + exp + "'", "<" + ptppath, ">" + tplpath, shell=True)
+        run('sed', exp, "<" + ptppath, ">" + tplpath, shell=True)
 
 def clean():
     autoclean()
