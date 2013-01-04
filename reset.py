d119ce71ef1df87cc5913558d312e6b0767eba83
Marcel Rodrigues <marcelgmr@gmail.com>
2013-01-04 16:18:40 -0200

    No longer using Cork.

--- reset.py
+++ reset.py
@@ -1,33 +1,3 @@
-import datetime
+import model
 
-from cork import Cork
-
-def reset():
-    cork = Cork('corked', initialize=True)
-
-    cork._store.roles['admin'] = 100
-    cork._store.roles['editor'] = 60
-    cork._store.roles['user'] = 50
-    cork._store._savejson('roles', cork._store.roles)
-
-    tstamp = str(datetime.datetime.utcnow())
-    username = password = 'admin'
-    cork._store.users[username] = {
-        'role': 'admin',
-        'hash': cork._hash(username, password),
-        'email_addr': username + '@localhost.local',
-        'desc': username + ' test user',
-        'creation_date': tstamp
-    }
-    username = password = 'adan'
-    cork._store.users[username] = {
-        'role': 'user',
-        'hash': cork._hash(username, password),
-        'email_addr': username + '@localhost.local',
-        'desc': username + ' test user',
-        'creation_date': tstamp
-    }
-    cork._store._save_users()
-
-if __name__ == '__main__':
-    reset()
+model.create_tables()
