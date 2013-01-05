5fd455165de88e6d67428e102fa857f389485f8e
Marcel Rodrigues <marcelgmr@gmail.com>
2013-01-04 23:00:41 -0200

    First login attempt.

--- webapp.py
+++ webapp.py
@@ -1,7 +1,19 @@
 import os
 import gettext
 
-from bottle import route, view, static_file, request, run
+from bottle import route, post, redirect, view, static_file, request, run
+
+import cryptacular.core
+import cryptacular.pbkdf2
+import cryptacular.bcrypt
+
+import peewee
+
+import model
+
+pbkdf2 = cryptacular.pbkdf2.PBKDF2PasswordManager()
+bcrypt = cryptacular.bcrypt.BCRYPTPasswordManager()
+delegator = cryptacular.core.DelegatingPasswordManager(preferred=pbkdf2, fallbacks=(bcrypt,))
 
 DEFAULT_LANG = 'en'
 LANGS = [DEFAULT_LANG]
@@ -37,7 +49,7 @@ def get_underline(domain, lang):
 def send_static(filename):
     return static_file(filename, root='static')
 
-@route('/')
+@route('/login')
 @view('login')
 def login():
     lang = request.query.lang
@@ -45,5 +57,21 @@ def login():
         lang = lang_from_header(request.headers.get("Accept-Language", ""))
     _ = get_underline("login", lang)
     return dict(_=_, lang=lang)
+
+@post('/login')
+def login_submit():
+    username = request.forms.get('username')
+    password = request.forms.get('password')
+    model.paladar_db.connect()
+    try:
+        user = model.User.get(model.User.handle == username)
+    except peewee.DoesNotExist:
+        user = None
+    if user is None:
+        redirect('/login')
+    ok = delegator.check(user.hashed_password, password)
+    if not ok:
+        redirect('/login')
+    return "Welcome {0}!".format(user.name)
 
 run(host='localhost', server='tornado', debug=True, reloader=True)
