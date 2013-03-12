3e937556fb8e80d4d2284357fd81575a76d271ca
Marcel Rodrigues <marcelgmr@gmail.com>
2013-03-12 11:59:06 -0300

    Add environment tests.

---
+++ test/test_env.py
@@ -0,0 +1,26 @@
+def test_beaker():
+    import beaker
+
+def test_bottle():
+    import bottle
+
+def test_cryptacular():
+    import cryptacular
+
+def test_feedparser():
+    import feedparser
+
+def test_pbkdf2():
+    import pbkdf2
+
+def test_peewee():
+    import peewee
+
+def test_polib():
+    import polib
+
+def test_memcached():
+    import memcache
+
+def test_tornado():
+    import tornado
